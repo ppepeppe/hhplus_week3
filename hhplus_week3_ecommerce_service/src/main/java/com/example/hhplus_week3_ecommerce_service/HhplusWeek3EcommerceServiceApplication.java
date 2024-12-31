1ec832e0e6ec30b9749172b3d502a1dbf78e0f02
unknown <155891@jnu.ac.kr>
2024-12-31 14:37:22 +0900

    초기 세팅

---
+++ hhplus_week3_ecommerce_service/src/main/java/com/example/hhplus_week3_ecommerce_service/HhplusWeek3EcommerceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.hhplus_week3_ecommerce_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusWeek3EcommerceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhplusWeek3EcommerceServiceApplication.class, args);
+	}
+
+}
---
+++ hhplus_week3_ecommerce_service/src/test/java/com/example/hhplus_week3_ecommerce_service/HhplusWeek3EcommerceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hhplus_week3_ecommerce_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusWeek3EcommerceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
