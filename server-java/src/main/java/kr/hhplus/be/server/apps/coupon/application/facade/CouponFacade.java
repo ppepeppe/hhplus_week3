92259ab6b616358961eb302eb11d85fb497d5ec9
ppepeppe <dmdsns@naver.com>
2025-02-07 00:58:32 +0900

    refact: 주석 제거

--- server-java/src/main/java/kr/hhplus/be/server/apps/coupon/application/facade/CouponFacade.java
+++ server-java/src/main/java/kr/hhplus/be/server/apps/coupon/application/facade/CouponFacade.java
@@ -1,12 +1,6 @@
 package kr.hhplus.be.server.apps.coupon.application.facade;
 
-import jakarta.transaction.Transactional;
 import kr.hhplus.be.server.apps.coupon.application.usecase.CouponUseCase;
-import kr.hhplus.be.server.apps.coupon.domain.models.UserCoupon;
-import kr.hhplus.be.server.apps.order.application.usecase.OrderUseCase;
-import kr.hhplus.be.server.apps.order.domain.models.dto.OrderItemDTO;
-import kr.hhplus.be.server.apps.order.domain.models.entity.Order;
-import kr.hhplus.be.server.apps.order.domain.models.entity.OrderItem;
 import kr.hhplus.be.server.common.util.RedisLockUtil;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RLock;
@@ -14,28 +8,15 @@ import org.redisson.api.RedissonClient;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
-import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 @Component
 @RequiredArgsConstructor
 public class CouponFacade {
     private final CouponUseCase couponUseCase;
-    private final RedisLockUtil redisLockUtil;
     private final RedissonClient redissonClient;
     private final RedisTemplate<String, Object> redisTemplate;
-//    public UserCoupon issueCoupon(Long userId, Long couponId) {
-//        String lockKey = generateLockKey(couponId);
-//        boolean acquired = redisLockUtil.acquireLock(lockKey, userId, 3, 10);
-//        if (!acquired) {
-//            throw new RuntimeException("쿠폰 발급 요청이 많아 처리되지 않았습니다. 다시 시도해주세요.");
-//        }
-//        try {
-//            return couponUseCase.couponIssuanceTransactional(userId, couponId);
-//        } finally {
-//            redisLockUtil.releaseLock(lockKey, userId);
-//        }
-//    }
+
 
     public void issueCoupon(Long userId, Long couponId) throws InterruptedException {
         String couponStockKey = "coupon:stock:" + couponId;
@@ -71,8 +52,5 @@ public class CouponFacade {
         } finally {
             lock.unlock();
         }
-    }
-    private String generateLockKey(Long couponId) {
-        return "lock:coupon:" + couponId;
     }
 }
