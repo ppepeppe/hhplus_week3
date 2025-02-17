e8f74f4e033ef6875cd8a0abc1cd6629d2d4e3a9
ppepeppe <dmdsns@naver.com>
2025-02-18 00:39:04 +0900

    kafka 추가

--- server-java/build.gradle.kts
+++ server-java/build.gradle.kts
@@ -49,6 +49,10 @@ dependencies {
 	// redisson
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 	implementation("org.redisson:redisson-spring-boot-starter:3.17.6")
+	// Kafka
+	implementation("org.springframework.kafka:spring-kafka")
+	// Kafka Test
+	testImplementation("org.springframework.kafka:spring-kafka-test")
 	// Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
---
+++ server-java/src/test/java/kr/hhplus/be/server/integration/KafkaIntegrationTest.java
@@ -0,0 +1,46 @@
+package kr.hhplus.be.server.integration;
+
+import kr.hhplus.be.server.apps.order.domain.models.entity.Order;
+import kr.hhplus.be.server.config.kafka.OrderEventConsumer;
+import kr.hhplus.be.server.config.kafka.OrderEventProducer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+@SpringBootTest
+class KafkaIntegrationTest {
+    @Autowired
+    private OrderEventProducer producer;
+
+    @Autowired
+    private OrderEventConsumer consumer;
+
+    @BeforeEach
+    void setUp() {
+        consumer.clearReceivedOrders();  // 각 테스트 전에 초기화
+    }
+
+    @Test
+    void whenSendMessage_thenShouldReceiveIt() throws Exception {
+        // given
+        Order order = Order.builder()
+                .orderId(1L)
+                .userId(1L)
+                .totalPaymentAmount(1000)
+                .build();
+
+        // when
+        producer.send(order);
+        Thread.sleep(1000);  // Consumer가 메시지를 처리할 시간 대기
+
+        // then
+        List<Order> receivedOrders = consumer.getReceivedOrders();
+        assertThat(receivedOrders.get(0))
+                .extracting(Order::getOrderId, Order::getUserId, Order::getTotalPaymentAmount)
+                .containsExactly(1L, 1L, 1000);
+    }
+}
