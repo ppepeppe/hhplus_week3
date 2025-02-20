07da825e6d9f18efef2e4b1717202562b4644da9
ppepeppe <dmdsns@naver.com>
2025-02-21 01:24:25 +0900

    feat: 스케쥴링 추가

--- server-java/src/main/java/kr/hhplus/be/server/ServerApplication.java
+++ server-java/src/main/java/kr/hhplus/be/server/ServerApplication.java
@@ -3,10 +3,11 @@ package kr.hhplus.be.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "kr.hhplus.be.server.apps")
-
+@EnableScheduling
 public class ServerApplication {
 
 	public static void main(String[] args) {
